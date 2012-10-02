f9e882ba5e0b4ac3dae01cfb4e4f64da2572fdac
Andy May <andyjmay@gmail.com>
2012-10-01 22:21:12 -0400

    Initial commit

---
+++ LyricsMaven/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace LyricsMaven {
+  public class BundleConfig {
+    public static void RegisterBundles(BundleCollection bundles) {
+      bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+      bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.unobtrusive*", "~/Scripts/jquery.validate*"));
+      bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+      bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap*", "~/Scripts/bootstrap-reponsive*"));
+      bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+    }
+  }
+}
